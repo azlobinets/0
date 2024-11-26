bd1d9d85caa8750ed7e9d2ed1824c96b88d24443
azlobinets <andreyross962@gmail.com>
2024-11-26 21:53:57 +1000

    Add files via upload

---
+++ Модуль1д.з. 4 .py
@@ -0,0 +1,20 @@
+#2.1
+my_string=input("Введите текст: ")
+
+#2.2
+print(len(my_string)) 
+
+#3.1
+print(my_string.upper())
+
+#3.2
+print(my_string.lower())
+
+#3.3
+print(my_string.replace(" ", ""))
+
+#3.4
+print(my_string[0])
+
+#3.5
+print(my_string[-1]) # Python поддерживает отрицательную индексацию, которая позволяет обращаться к элементам строки с конца.
